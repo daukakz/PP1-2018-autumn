11e2cb3d9ff939220229551051a1676332951bda
Nur99 <nayapbergen@gmail.com>
2018-08-30 18:04:03 +0600

    Update B.cpp

--- labs/lab1/B.cpp
+++ labs/lab1/B.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 int main(){
   int n;
   cin>>n;
   
-  printf("The next number for the number %d is %d.\n", n, n+1);
-  printf("The previous number for the number %d is %d.\n", n, n-1);
-
+  
+  cout<<"The next number for the number "<<n<<" is "<<n+1<<"."<<endl;
+  cout<<"The previous number for the number "<<n<<" is "<<n-1<<"."<<endl;
 }
