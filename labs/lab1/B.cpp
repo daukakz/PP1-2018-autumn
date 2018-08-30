dbd19a9a865486b4b85ec0570f055a50984f4b64
Nur99 <nayapbergen@gmail.com>
2018-08-30 17:58:09 +0600

    Update B.cpp

--- labs/lab1/B.cpp
+++ labs/lab1/B.cpp
@@ -5,7 +5,7 @@ int main(){
   int n;
   cin>>n;
   
-  printf("The next number for number %d is %d.\n", n, n+1);
-  printf("The previous number for number %d is %d.\n", n, n-1);
+  printf("The next number for the number %d is %d.\n", n, n+1);
+  printf("The previous number for the number %d is %d.\n", n, n-1);
 
 }
