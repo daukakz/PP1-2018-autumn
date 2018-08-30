4f3f679c9a6adc7491024a391a30c658eb4e3451
Nur99 <nayapbergen@gmail.com>
2018-08-30 17:45:29 +0600

    Create B.cpp

---
+++ labs/lab1/B.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  
+  printf("The next number for number %d is %d.\n", n, n+1);
+  printf("The previous number for number %d is %d.\n", n, n-1);
+
+}
