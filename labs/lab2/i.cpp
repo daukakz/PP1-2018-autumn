212a8806eadd783ea4ed04cbb9bc0bbf9f304429
Nurymzhan <nayapbergen@gmail.com>
2018-09-04 22:03:47 +0600

    Create i.cpp

---
+++ labs/lab2/i.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+     
+    int n;
+    cin>>n;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin>>x;
+        if(x % 10 == 0)ans++;
+    }   
+    cout<<zeros;
+}
