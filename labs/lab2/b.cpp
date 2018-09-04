793537618e8a8765e52e3b35f2706bf6096b35d6
Nurymzhan <nayapbergen@gmail.com>
2018-09-04 21:29:19 +0600

    Update b.cpp

--- labs/lab2/b.cpp
+++ labs/lab2/b.cpp
@@ -3,8 +3,8 @@
  int main(){
   int n;
   cin>>n;
-
-  if(n % 100 == 0 || n % 4 != 0)cout<<"NO";
+  if(n % 400 == 0)cout<<"YES";
+  else if(n % 100 == 0 || n % 4 != 0)cout<<"NO";
   else cout<<"YES";
  
  }
