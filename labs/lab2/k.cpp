786d7493584a830a44fa1bbe7835ad1c9212a39a
Nurymzhan <nayapbergen@gmail.com>
2018-09-04 22:12:36 +0600

    Create k.cpp

---
+++ labs/lab2/k.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+     
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i*i <= n){
+        cout<<i*i<<endl;
+	      i++;
+    }
+}
