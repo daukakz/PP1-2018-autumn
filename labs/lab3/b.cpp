dd0c6c32aa35ab9717eee93415450b11ef2327e3
Nurymzhan <nayapbergen@gmail.com>
2018-09-11 10:23:09 +0600

    Create b.cpp

---
+++ labs/lab3/b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    int a[n];
+    
+    for(int i = 0; i < n; i++)cin>>a[i];
+    
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] > 0)ans++;   
+    }
+    cout<<ans;
+}
