8b38df42e2f8742cadb9052944b27d123eebe8ed
Nurymzhan <nayapbergen@gmail.com>
2018-09-12 20:11:53 +0600

    Update c.cpp

--- labs/lab2/c.cpp
+++ labs/lab2/c.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    if(n%2 != 0) cout<<"Super";
-    else{
-        if(n >= 2 && n <= 5)cout<<"Not Super";
-        else if(n >= 6 && n <= 20)cout<<"Super";
-        else cout<<"Not Super";
-    }
-}
+ #include <bits/stdc++.h>
+ using namespace std;
+ int main(){
+  int n;
+  cin>>n;
+  if(n % 400 == 0)cout<<"YES";
+  else if(n % 100 == 0 || n % 4 != 0)cout<<"NO";
+  else cout<<"YES";
+ 
+ }
