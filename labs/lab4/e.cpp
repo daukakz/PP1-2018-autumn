6386da74e6262882938c5dcae44929efde06648b
Nurymzhan <nayapbergen@gmail.com>
2018-10-01 16:32:52 +0600

    Update e.cpp

--- labs/lab4/e.cpp
+++ labs/lab4/e.cpp
@@ -1,28 +1,18 @@
 #include <iostream>
 using namespace std;
-int main () {
 
+int main(){
     int n;
     cin>>n;
-
-    n++;
-    string a[n][n];
-
-    for(int i = 1; i < n; i++){
-        for(int j = 0; j < i; j++){
-            a[i][j] = "[*]";    
-        }
-    }
-
     
-
-    for(int i = 1; i < n; i++){
-        for(int j = 0; j < i; j++){
-            cout<<a[i][j];    
+    for(int i = 0; i < n; i++){
+        int j = i+1;
+        while(j != 0){
+            cout<<"[*]";   
+            j--;
         }
         cout<<endl;
     }
 
 
-
-}   
+}
