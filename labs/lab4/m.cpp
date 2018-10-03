f94a43b5bd6e1290b2eeccedecbf8b21722b9775
Nurymzhan <nayapbergen@gmail.com>
2018-10-03 23:06:50 +0600

    Update m.cpp

--- labs/lab4/m.cpp
+++ labs/lab4/m.cpp
@@ -1,37 +1,60 @@
-#include <iostream>
-using namespace std;
-int main(){
     int n;
     cin>>n;
+    
     int a[n][n];
-    for(int i=0;i<n;i++)
-    	for(int j=0;j<n;j++)a[i][j]=0;
-    a[0][0]=1;
-    a[0][1]=2;
-    int i=0,j=1;
-    int x=2;
-    while(x!=n*n){
-    if(a[i][j-1]==x-1){
-        if(!a[i][j+1]&&j+1<n)j++;
-    	else i++;
+    
+    for(int i = 0; i < n; i++){
+    	for(int j = 0; j < n; j++){
+    	    a[i][j] = 0;   
+    	}
     }
-    else if(a[i-1][j]==x-1){
-        if(!a[i+1][j]&&i+1<n)i++;
-        else j--;
+    a[0][0] = 1;
+    a[0][1] = 2;
+    
+    int i = 0, j = 1;
+    int x = 2;
+    
+    while(x != n*n){
+        if(a[i][j-1] == x-1){
+            if(a[i][j+1] == 0 && j+1<n){
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        else if(a[i-1][j] == x-1){
+            if(a[i+1][j] == 0 && i+1 < n){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        else if(a[i][j+1] == x-1){
+            if(a[i][j-1] == 0 && j > 0){
+                j--;
+            }
+            else{
+                i--;
+            }
+        }
+        else {
+            if(a[i-1][j] == 0 && i > 0){
+                i--;
+            }
+            else{
+                j++;
+            }
+        }
+        x++;
+        a[i][j] = x;
     }
-    else if(a[i][j+1]==x-1){
-        if(!a[i][j-1]&&j>0)j--;
-        else i--;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
     }
-    else {
-        if(!a[i-1][j]&&i>0)i--;
-        else j++;
-    }
-    x++;
-    a[i][j]=x;
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++)cout<<a[i][j]<<" ";
-        cout<<endl;}
     return 0;
 }
