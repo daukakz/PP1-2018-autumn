b97f1f382bf33d34a754585290ad3babe0e4d392
Nurymzhan <nayapbergen@gmail.com>
2018-10-05 15:56:16 +0600

    Update a.cpp

--- labs/lab5/a.cpp
+++ labs/lab5/a.cpp
@@ -3,23 +3,18 @@ using namespace std;
 int main(){
     string s;
     cin>>s;
-    
     /*
-    
         https://cdn-images-1.medium.com/max/1600/1*DdgD00dAdXggzMdWDt7GSA.png
-        
-        look to ascii codes of small letters and capital letters of c++
-        capitalLetter = smallLetter - 32;
-        
-        
     */
+    int cap = 0, sm = 0;
     for(int i = 0; i < s.size(); i++){
         if(s[i] >= 'a' && s[i] <= 'z'){
-            s[i] -= 32;   
+            sm++;  
+        }
+        else{
+            cap++;   
         }
     }
     
-    
-    cout<<s;
-    
+    cout<<sm<<' '<<cap;    
 }
